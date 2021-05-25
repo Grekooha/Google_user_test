95b954fdf2deb02cfa4332459419726ea576808c
Vyacheslav Grishin <vyacheslav.grishin@valamis.com>
2021-05-25 17:08:46 +0300

    Added AnyFlatSpec

--- src/main/scala/Google/Google.scala
+++ src/main/scala/Google/Google.scala
@@ -1,5 +1,6 @@
 package Google
 
+import com.codeborne.selenide.Selectors.byText
 import com.codeborne.selenide.Selenide.{$, $$, open}
 import com.codeborne.selenide.{CollectionCondition, Condition}
 import org.openqa.selenium.By
@@ -7,6 +8,9 @@ import org.openqa.selenium.By
 class Google {
   val cssSelector: String = "#search .g"
   val url: String = "http://www.google.com"
+  private val username = "test.grekooha"
+  private val password = "Qazxc31367"
+
 
   def openGoogle(): Unit =
     open(url)
@@ -22,5 +26,15 @@ class Google {
     $(cssSelector)
       .shouldHave(Condition.text(assertHasResultText))
       .shouldBe(Condition.visible)
+  }
+
+  def pressButton(buttonName:String): Unit = {
+    $(byText(buttonName))
+      .pressEnter()
+  }
+
+  def login() {
+    $("#identifierId").`val`(username).pressEnter
+    $("#password input").`val`(password).pressEnter
   }
 }
--- src/test/scala/Google/GooglePage.scala
+++ src/test/scala/Google/GooglePage.scala
@@ -5,13 +5,16 @@ import org.scalatest.flatspec.AnyFlatSpec
 class GooglePage extends AnyFlatSpec {
   private val page = new Google()
 
-  "The method" should "open the site" in {
+  it should "open the site and input text and press enter" in {
     page.openGoogle()
-  }
-  it should "input text and press enter" in {
     page.inputText("selenide")
   }
   it should "assert has the result" in {
     page.assertHasResult("Selenide: concise UI tests in Java")
+  }
+ it should "log in" in {
+    page.openGoogle()
+    page.pressButton("Sign in")
+    page.login()
   }
 }
